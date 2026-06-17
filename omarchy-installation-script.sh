2ff5d28348a1f41528bb8cf13e67feceb06eb67d
Techyiola <eashanpoddar16@gmail.com>
2026-06-17 23:20:30 +0530

    Update omarchy-installation-script.sh

--- omarchy-installation-script.sh
+++ omarchy-installation-script.sh
@@ -13,6 +13,9 @@ sudo pacman -Rns $(pacman -Qdtq)
 # Install a darker theme for omarchy
 # omarchy-theme-install https://github.com/atif-1402/omarchy-latchdark-theme.git
 
+# Waybar theme from HANCORE-linux project 2.9b theme
+git clone https://github.com/HANCORE-linux/waybar-themes.git /tmp/repo && cp -rf /tmp/repo/config/V2.9b/. ~/.config/waybar && rm -rf /tmp/repo && omarchy-restart-waybar
+
 
 # Delete the crap 
 rm -rf ~/.cache/*
